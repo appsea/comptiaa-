36ce17d77d0fc22d87823fad651e06d3ffd39854
appsea <girase.rakesh@gmail.com>
2019-06-03 21:48:23 +0100

    AAB Checks

--- app/services/ad.service.ts
+++ app/services/ad.service.ts
@@ -22,7 +22,7 @@ export class AdService {
         this._showAd = showAd;
     }
 
-    static _testing = false;
+    static _testing = true;
 
     static getInstance(): AdService {
         return AdService._instance;
