e5aeacb5129db0fb2ce0002326740fa6c0dcc002
appsea <teameaapps@gmail.com>
2020-10-09 22:08:18 +0100

    Upgrading the dependencies

--- references.d.ts
+++ references.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="./node_modules/@nativescript/core/references.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path="./node_modules/@nativescript/core/index.d.ts" /> Needed for autocompletion and compilation.
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 /// <reference path="./node_modules/nativescript-purchase/nativescript-purchase.d.ts" />
