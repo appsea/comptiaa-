f35417c22f83e84049e58cb08f5a900b84824701
appsea <teameaapps@gmail.com>
2020-10-12 16:00:44 +0100

    Removed tns-core-modules and fixed compilation issues

--- references.d.ts
+++ references.d.ts
@@ -1,3 +1,3 @@
 /// <reference path="./node_modules/@nativescript/core/index.d.ts" /> Needed for autocompletion and compilation.
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
 /// <reference path="./node_modules/nativescript-purchase/nativescript-purchase.d.ts" />
